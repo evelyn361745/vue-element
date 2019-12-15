c283999dd6548723e44f362bf99ece583916e8ce
evelyn361745 <1139447839@qq.com>
2019-12-15 18:45:41 +0800

    新增footguide页面

---
+++ src/components/footer/footGuide.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="footguide">
+        <van-tabbar v-model="active">
+            <van-tabbar-item 
+                name="msite" 
+                icon="shop-o"
+                :to="'/msite?geohash='+ geohash">
+                外卖
+            </van-tabbar-item>
+            <van-tabbar-item 
+                name="search" 
+                icon="search"
+                :to="'/search/' + geohash">
+                搜索
+            </van-tabbar-item>
+            <van-tabbar-item 
+                name="order" 
+                icon="records"
+                to="/order">
+                订单
+            </van-tabbar-item>
+            <van-tabbar-item 
+                name="profile" 
+                icon="contact"
+                to="/profile">
+                我的
+            </van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            active: 'msite',
+            geohash: '',
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/page/msite/msite.vue
+++ src/page/msite/msite.vue
@@ -1,15 +1,19 @@
 <template>
     <div id="msite">
-        
+        <foot-guide></foot-guide>
     </div>
 </template>
 
 <script>
+import footGuide from '../../components/footer/footGuide.vue'
 export default {
     data() {
         return {
-
+            geohash: '', // city页面传递过来的地址geohash
         }
+    },
+    components: {
+        footGuide
     }
 }
 </script>
