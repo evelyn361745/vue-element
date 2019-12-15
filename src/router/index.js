41e391e5c2ab8e3c1198d1534b6045552ee36179
evelyn361745 <1139447839@qq.com>
2019-12-15 18:14:43 +0800

    新增页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '../page/home/home'
-import login from '../page/login/login'
-import city from '../page/city/city'
-import msite from '../page/msite/msite'
 Vue.use(Router)
 
+const home = r => require.ensure([], () => r(require('../page/home/home')),'home')
+const login = r => require.ensure([], () => r(require('../page/login/login')),'login')
+const city = r => require.ensure([], () => r(require('../page/city/city')),'city')
+const msite = r => require.ensure([], () => r(require('../page/msite/msite')),'msite')
+const order = r => require.ensure([], () => r(require('../page/order/order')),'order')
+const profile = r => require.ensure([], () => r(require('../page/profile/profile')),'profile')
+const search = r => require.ensure([], () => r(require('../page/search/search')),'search')
 export default new Router({
   routes: [
     {
@@ -30,6 +33,18 @@ export default new Router({
       meta: {
         keepAlive: true
       }
+    },
+    {
+      path: '/search/:geohash',
+      component: search
+    },
+    {
+      path: '/profile',
+      component: profile
+    },
+    {
+      path: '/order',
+      component: order
     }
   ]
 })
